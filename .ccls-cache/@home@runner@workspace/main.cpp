d5bb2e024378fb53bc53de23d822a7819f6f1ba9
Sally Schaffner <151073463+SallySchaffner@users.noreply.github.com>
2025-05-26 00:06:35 +0000

    Added vector modification example

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -1,5 +1,60 @@
-// Chapter 7 Example Programs 
-// Multiple programs each on a separate branch
+// This program is a driver that tests a function comparing the
+// contents of two int arrays.
 #include <iostream>
+#include <vector>    // ---- Include the vector library
+using namespace std;
 
-int main() { std::cout << "Hello World!\n"; }
+// Function Prototype
+//bool testPIN(const int[], const int[], int);
+//---- Modify testPIN to accept vectors instead of arrays
+//---- (Don't need to send in the size as the vector keeps track of that for you)
+bool testPIN(const vector<int>, const vector<int>);
+
+int main()
+{
+    //const int NUM_DIGITS = 7; // Number of digits in a PIN
+    /* int pin1[NUM_DIGITS] = {2, 4, 1, 8, 7, 9, 0}; // Base set of values.
+    int pin2[NUM_DIGITS] = { 2, 4, 6, 8, 7, 9, 0 }; // Only 1 element is
+    // different from pin1.
+    int pin3[NUM_DIGITS] = { 1, 2, 3, 4, 5, 6, 7 }; // All elements are
+    // different from pin1.*/
+    // ---- Use vectors instead of arrays
+    vector<int> pin1 = { 2, 4, 1, 8, 7, 9, 0 };
+    vector<int> pin2 = { 2, 4, 6, 8, 7, 9, 0 };
+    vector<int> pin3 = { 1, 2, 3, 4, 5, 6, 7 };
+
+
+    if (testPIN(pin1, pin2))
+        cout << "ERROR: pin1 and pin2 report to be the same.\n";
+    else
+        cout << "SUCCESS: pin1 and pin2 are different.\n";
+
+    if (testPIN(pin1, pin3))
+        cout << "ERROR: pin1 and pin3 report to be the same.\n";
+    else
+        cout << "SUCCESS: pin1 and pin3 are different.\n";
+
+    if (testPIN(pin1, pin1))
+        cout << "SUCCESS: pin1 and pin1 report to be the same.\n";
+    else
+        cout << "ERROR: pin1 and pin1 report to be different.\n";
+    return 0;
+}
+
+//******************************************************************
+// The following function accepts two int arrays. The arrays are   *
+// compared. If they contain the same values, true is returned.    *
+// If the contain different values, false is returned.             *
+//******************************************************************
+
+//bool testPIN(const int custPIN[], const int databasePIN[], int size)
+// ---- Change the header to accept a vector instead of an array
+bool testPIN(const vector<int> custPIN, const vector<int> databasePIN)
+{
+    for (int index = 0; index < custPIN.size(); index++)   // Use the size value stored in the vector
+    {
+        if (custPIN[index] != databasePIN[index])
+            return false; // We've found two different values.
+    }
+    return true; // If we make it this far, the values are the same.
+}
